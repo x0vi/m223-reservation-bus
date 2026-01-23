580af0e2da869dafce2c39448b555fa37a7d7633
xovi <xaviercravo@proton.me>
2026-01-23 15:22:10 +0100

    Mise à jour de DB + correction de noms

--- src/JdbcConnectionTest.java
+++ src/JdbcConnectionTest.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnectionTest {
-    private static final String URL = "jdbc:mysql://localhost:22306/projet_db";
+    private static final String URL = "jdbc:mysql://localhost:22306/gbc";
     private static final String USER = "projet_user";
     private static final String PASSWORD = "projet_pwd";
 
--- src/db.sql
+++ src/db.sql
@@ -1,37 +1,34 @@
 START TRANSACTION;
+USE gbc;
 
-CREATE TABLE IF NOT EXISTS `employe` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nom` VARCHAR(30),
-    `prenom` VARCHAR(30),
-    CONSTRAINT `pk_table_1_id` PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS t_vehicule (
+    plaque VARCHAR(8) PRIMARY KEY,
+    marque VARCHAR(50) ,
+    modele VARCHAR(50) ,
+    capacite_max INT (2)
 );
 
 
-
-CREATE TABLE IF NOT EXISTS `vehicule` (
-    `plaques` VARCHAR(8) NOT NULL,
-    `capacite_max` INT,
-    `marque` VARCHAR(50),
-    `modele` VARCHAR(50),
-    CONSTRAINT `pk_table_2_id` PRIMARY KEY (`plaques`)
+CREATE TABLE IF NOT EXISTS t_employe (
+    id_employe INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    prenom VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS t_reservation (
+    id_reservation INT AUTO_INCREMENT PRIMARY KEY,
+    date_reservation DATE NOT NULL,
+    plaque VARCHAR(8) NOT NULL,
+    id_employe INT NOT NULL,
 
+    FOREIGN KEY (plaque)
+        REFERENCES t_vehicule(plaque)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
 
-CREATE TABLE IF NOT EXISTS `reservation` (
-    `id` INT NOT NULL,
-    `date` DATE,
-    `plaques` VARCHAR(8),
-    `capacite_max` INT,
-    `id_employe` INT,
-    CONSTRAINT `pk_table_3_id` PRIMARY KEY (`id`)
+    FOREIGN KEY (id_employe)
+        REFERENCES t_employe(id_employe)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
-
-
--- Foreign key constraints
-ALTER TABLE `employe` ADD CONSTRAINT `fk_employe_id` FOREIGN KEY(`id`) REFERENCES `reservation`(`id_employe`);
-ALTER TABLE `vehicule` ADD CONSTRAINT `fk_vehicule_plaques` FOREIGN KEY(`plaques`) REFERENCES `reservation`(`plaques`);
-ALTER TABLE `vehicule` ADD CONSTRAINT `fk_vehicule_capacite_max` FOREIGN KEY(`capacite_max`) REFERENCES `reservation`(`capacite_max`);
-
 COMMIT;
