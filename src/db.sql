fdbdfca321234ce106840d9c225d8aa04b767e91
scorpions1269 t'emmerde <bulliardg@gmail.com>
2026-01-30 14:34:58 +0100

    Version new juste

--- src/db.sql
+++ src/db.sql
@@ -1,1 +1,36 @@
-git add
+START TRANSACTION;
+USE gbc;
+
+CREATE TABLE IF NOT EXISTS t_vehicule (
+    plaque VARCHAR(8) PRIMARY KEY,
+    marque VARCHAR(50) ,
+    modele VARCHAR(50) ,
+    capacite_max INT (2)
+);
+
+
+CREATE TABLE IF NOT EXISTS t_employe (
+    id_employe INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    prenom VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS t_reservation (
+    id_reservation INT AUTO_INCREMENT PRIMARY KEY,
+    date_reservation DATE NOT NULL,
+    plaque VARCHAR(8) NOT NULL,
+    id_employe INT NOT NULL,
+
+    FOREIGN KEY (plaque)
+        REFERENCES t_vehicule(plaque)
+        ON DELETE RESTRICT
+        /*si le véhicules est dans une reservation il ne peut pas etre supprimé*/
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (id_employe)
+        REFERENCES t_employe(id_employe)
+        ON DELETE RESTRICT
+        /*si l'employé est dans une reservation il ne peut pas etre supprimé*/
+        ON UPDATE CASCADE
+);
+COMMIT;
